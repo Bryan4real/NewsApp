e4ff244240f4b76c37e907dabc339fcbe01a8faf
Brian Mwiruki <mwiruki@outlook.com>
2022-03-14 12:33:44 +0300

    newsapi topheading endpoint method initialization

--- src/app/service/newsapiservices.service.ts
+++ src/app/service/newsapiservices.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsapiservicesService {
 
-  constructor() { }
+  constructor(private _http:HttpClient) { }
+
+  //newsApiUrl end point
+  newsApiUrl = "https://newsapi.org/v2/top-headlines?country=us&apiKey=8249113b94484c208731d89b5c6413ed";
+
+  //topheading method for topheading component endpoint data
+
+  topHeading():Observable<any>
+  {
+    return this._http.get(this.newsApiUrl);
+  }
 }
